45b45298a038ebbeca599ff31af6aa8d0e28cb88
Eda Kalaycioglu <edakalaycioglu@gmail.com>
2024-03-26 23:28:03 +0300

    entities

---
+++ src/main/java/com/workintech/demo/entity/Account.java
@@ -0,0 +1,20 @@
+package com.workintech.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "account", schema = "bank")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+}
